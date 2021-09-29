0c115b79d240ec5627154c1d3a085c1df8596215
nathancao <ncao2023@chadwickschool.org>
2021-09-29 11:45:35 -0700

    feat: Add steps 1 and 2 of 11. Shared Modules.
    Received error, 'A merge conflicted on path /src/app/shared/shell/shell.component.html' when running ng g component shared/shell --export

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ShellComponent } from './shared/shell/shell.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShellComponent
-  ],
+    AppComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,8 +26,9 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
-  ],
+    MatListModule,
+    SharedModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
