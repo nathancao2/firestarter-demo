85d446d7fcc187b77f39b50b5c5de16fd7d00917
nathancao <ncao2023@chadwickschool.org>
2021-09-30 20:38:10 -0700

    feat: implement firebase, errors persist, need to do a quick checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
     ],
   imports: [
     BrowserModule,
@@ -27,7 +31,8 @@ import { SharedModule } from './shared/shared.module';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
     ],
   providers: [],
   bootstrap: [AppComponent]
